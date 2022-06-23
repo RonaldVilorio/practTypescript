5c72ace4ff23c3ab3173d2e4cf87c209afe6bb7c
RonaldVilorio <ronvilorio@gmail.com>
2022-06-23 18:53:02 -0400

    annotations for 3 different function types

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -2,3 +2,9 @@ const add = (a:number,b:number): number=>{
     return a+b
 
 }
+function divide(a:number,b:number): number{
+    return a/b
+}
+const multiply = function(a:number,b:number): number{
+    return a*b
+}
