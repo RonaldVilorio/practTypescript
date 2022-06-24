6d52009a7c22e69a759fe2acffab6dda05986eb0
RonaldVilorio <ronvilorio@gmail.com>
2022-06-24 07:41:41 -0400

    destructuring with annotations

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -8,3 +8,16 @@ function divide(a:number,b:number): number{
 const multiply = function(a:number,b:number): number{
     return a*b
 }
+// destructuring
+const todaysWeather = {
+    date:new Date(),
+    weather:'sunny'
+}
+const logWeather = (forecast:{date:Date,weather:string}):void=>{
+    console.log(forecast.date, forecast.weather)
+}
+logWeather(todaysWeather)
+
+const logWeather2 =({date,weather}:{date:Date,weather:string}):void=>{
+    console.log(date,weather)
+}
