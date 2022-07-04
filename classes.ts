f961cc6eabbcf0c6f4500fb636b8d869b921e369
RonaldVilorio <ronvilorio@gmail.com>
2022-07-04 16:11:47 -0400

    where to use classes f

--- classes.ts
+++ classes.ts
@@ -1,12 +1,14 @@
 class Vehicle{
-    // drive(): void{
-    //     console.log("chugga chugga")
-    // }
+
+    constructor(public color:string){}    
     protected honk(): void{
         console.log('honk');
     }
 }
 class Car extends Vehicle{
+    constructor(public wheels:number,color:string){
+        super(color);
+    }
     private drive():void{
         console.log("chugga")
     }
@@ -14,9 +16,9 @@ class Car extends Vehicle{
         this.drive()
         this.honk();
     }
-
 }
-const car2 = new Vehicle
-const vehicle = new Vehicle()
-const car = new Car();
-car.startDrivingProcess()
+// const car2 = new Vehicle
+const car = new Car(3,'blue');
+// car.startDrivingProcess()
+const vehicle = new Vehicle('orange')
+console.log(vehicle.color,car.color,car.wheels)
